390da2e8fa3a1616239b9bc4b424a321f19f9747
erwinaja <erwinsonaja@gmail.com>
2024-10-03 09:10:22 +0700

    first commit

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class Baseurl {
+  static String tambah = "http://192.168.43.80/biodata/create.php";
+  static String data = "http://192.168.43.80/biodata/list.php";
+  static String lihat = "http://192.168.43.80/biodata/details.php";
+  static String edit = "http://192.168.43.80/biodata/update.php";
+  static String hapus = "http://192.168.43.80/biodat  a/delete.php";
+}
